47b81c9ef8567af63fe0b8c07ec240bd8bfe2e96
Heesoo Mac <heesoo4199@gmail.com>
2018-07-05 16:08:59 -0700

    Firebase authentication and auth based component serving using states in RN, done.

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,11 +1,57 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header, Button, Spinner, CardSection } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: null };
+
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyCEzCHy7hJ0aFsKwW0-bahg-7KEz5cqg0U',
+      authDomain: 'react-native-auth-14b29.firebaseapp.com',
+      databaseURL: 'https://react-native-auth-14b29.firebaseio.com',
+      projectId: 'react-native-auth-14b29',
+      storageBucket: 'react-native-auth-14b29.appspot.com',
+      messagingSenderId: '550680881847'
+    });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+          this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <CardSection>
+            <Button onPress={() => firebase.auth().signOut()}>
+              Logout
+            </Button>
+          </CardSection>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return (
+          <CardSection>
+            <Spinner size="large" />
+          </CardSection>
+        );
+    }
+  }
+
   render() {
     return (
       <View>
-        <Text>An App!</Text>
+        <Header headerText="Authentication" />
+        {this.renderContent()}
       </View>
     );
   }
